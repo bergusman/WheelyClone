b9b1bda1178b30ef521a19b6b02ccc556d568efa
Vitaly Berg <bergusman@gmail.com>
2015-04-18 13:59:32 +0300

    Initial Commit

---
+++ WheelyClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WheelyClone
+//
+//  Created by Vitaly Berg on 18/04/15.
+//  Copyright (c) 2015 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
